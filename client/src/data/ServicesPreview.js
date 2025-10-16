3c57c3b29fd0527f4de1f69c4c996fefdcadb0f5
Dat The Loac <dloac@wpi.edu>
2025-10-16 14:28:03 -0400

    updated services

--- client/src/data/ServicesPreview.js
+++ client/src/data/ServicesPreview.js
@@ -2,31 +2,37 @@ export const CATEGORIES_PREVIEW = [
   {
     slug: "manicure",
     title: "Manicure",
-    img: "/assets/services/manicure.jpg",
+    img: "/client/public/assets/services/manicure.jpg",
     snippet: "Classic, gel, and signature manicures."
   },
   {
     slug: "pedicure",
     title: "Pedicure",
-    img: "/assets/services/pedicure.jpg",
+    img: "/client/public/assets/services/pedicure.jpg",
     snippet: "From classic to deluxe spa pedicures."
   },
   {
     slug: "dipping",
     title: "Dipping Powder",
-    img: "/assets/services/dipping.jpg",
+    img: "/client/public/assets/services/dippingPowder.jpg",
     snippet: "Durable color with a lightweight feel."
   },
   {
     slug: "acrylic",
     title: "Acrylic / Color Powder",
-    img: "/assets/services/acrylic.jpg",
+    img: "/client/public/assets/services/acrylic.jpg",
     snippet: "Full sets, refills, and ombré designs."
   },
   {
     slug: "gelx",
     title: "Gel X / Builder",
-    img: "/assets/services/gelx.jpg",
+    img: "/client/public/assets/services/gelX.jpg",
     snippet: "Extensions and builder gel options."
+  },
+  {
+    slug: "kids",
+    title: "Kid Services (Under 9)",
+    img: "/client/public/assets/services/kidsService.jpg",
+    snippet: "Kids manicures and pedicures."
   }
 ]
